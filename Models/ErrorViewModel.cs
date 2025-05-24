a0d052a986028e97a367be4ba67f142d8501d975
PhamVanTu0309 <tuphamvan203@gmail.com>
2025-05-24 11:23:09 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PhamVanTu_2121050628_KiemTra_2425.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}

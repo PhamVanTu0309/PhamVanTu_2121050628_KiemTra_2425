bdde1a9a763a8a0e4a21f956042115f42e933f95
PhamVanTu0309 <tuphamvan203@gmail.com>
2025-05-24 11:32:53 +0700

    Add_Package

--- obj/Debug/net9.0/PhamVanTu_2121050628_KiemTra_2425.AssemblyInfo.cs
+++ obj/Debug/net9.0/PhamVanTu_2121050628_KiemTra_2425.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhamVanTu_2121050628_KiemTra_2425")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0d052a986028e97a367be4ba67f142d8501d975")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhamVanTu_2121050628_KiemTra_2425")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhamVanTu_2121050628_KiemTra_2425")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
